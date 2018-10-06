6f25d394d6c707593fecd0681bf82ae4717ba327
Sophie Gu <sophieqgu@gmail.com>
2018-10-06 15:56:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,8 @@
 
 def countdown (number)
   while number > 0 
-    puts "#{number} SECOND(S)!
+    puts "#{number} SECOND(S)!"
     number -= 1
   end 
-  puts "HAPPY NEW YEAR!"
-end
+  "HAPPY NEW YEAR!"
+end 
